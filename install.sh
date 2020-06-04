f106b229b7206488914db5b3865262685d0a73ca
Anthony T. Lannutti <Anthony.Lannutti@JBHunt.com>
2020-06-04 09:20:04 -0500

    Added Kali Linux support

--- install.sh
+++ install.sh
@@ -10,11 +10,11 @@ basename=$(dirname $(readlink -f ./install.sh))
 if [ -f /etc/os-release ]; then
   . /etc/os-release
   case $ID in
-    ubuntu|debian)
-      apt install -y $($basename/packages)
+    ubuntu|debian|kali)
+      sudo apt install -y $($basename/packages)
     ;;
     arch)
-      pacman -S --noconfirm $($basename/packages)
+      sudo pacman -S --noconfirm $($basename/packages)
     ;;
     *)
       >&2 echo "Unsupported OS: $NAME"
@@ -26,8 +26,31 @@ else
   exit 1
 fi
 
+echo 'Select one of these shells to be your default shell'
+grep ^/bin /etc/shells
+read shell;
+chsh --shell $shell $USER
+
+if [ ! -f $HOME/.ssh/id_rsa ] && [ ! -f $HOME/.ssh/id_rsa.pub ]; then
+  echo -n 'What is the email address for you SSH key: '
+  read ssh_email
+  ssh-keygen -t rsa -f $HOME/.ssh/id_rsa -b 4096 -C $ssh_email
+fi
+
+if [ -z "$(git config user.email)" ]; then
+  echo -n 'What is the email address you want to use for git: '
+  read git_user_email
+  git config --global user.email "$git_user_email"
+fi
+
+if [ -z "$(git config user.name)" ]; then
+  echo -n 'What is the name you want to use for git: '
+  read git_user_name
+  git config --global user.name "$git_user_name"
+fi
+
 if [ ! -d $HOME/.oh-my-zsh ]; then
-  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+  sh -c "$(curl -fskSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 fi
 
 for file in .vimrc .zshenv .zshrc .tmux.conf; do
