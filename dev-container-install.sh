e1b2833f07665da125d5e6a8dee3e1753d2abf8c
Anthony T. Lannutti <Anthony.Lannutti@JBHunt.com>
2020-06-09 19:54:31 -0500

    Simplified dev-container-install.sh

--- dev-container-install.sh
+++ dev-container-install.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
-command='./install.sh --no-interactive'
 if [ "$(id -u)" = 0 ]; then
   ./install.sh --no-interactive
 elif [ ! -z $(command -v sudo) ] && [ "$(sudo -v)" = 0 ]; then
-  sudo -H $command
+  sudo -H ./install.sh --no-interactive
 else
   >&2 echo "Cannot run as non-root user without sudo priveledges"
   exit 1;
