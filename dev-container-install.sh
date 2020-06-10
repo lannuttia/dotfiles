46a2ebfe8cdc6f1678bae0716ea37d738ed2f012
Anthony T. Lannutti <Anthony.Lannutti@JBHunt.com>
2020-06-09 19:34:18 -0500

    Fixed a negative logic bug in dev-container-install.sh

--- dev-container-install.sh
+++ dev-container-install.sh
@@ -2,7 +2,7 @@
 command='./install.sh --no-interactive'
 if [ "$(id -u)" = 0 ]; then
   ./install.sh --no-interactive
-elif [ -z $(command -v sudo) ] && [ "$(sudo -v)" = 0 ]; then
+elif [ ! -z $(command -v sudo) ] && [ "$(sudo -v)" = 0 ]; then
   sudo -H $command
 else
   >&2 echo "Cannot run as non-root user without sudo priveledges"
