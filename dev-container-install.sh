be70d74ff976274d42e84dc81dae242e49a95096
Anthony T. Lannutti <Anthony.Lannutti@JBHunt.com>
2020-06-09 20:08:47 -0500

    Fixed another bug in development container installation script

--- dev-container-install.sh
+++ dev-container-install.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
 if [ "$(id -u)" = 0 ]; then
   ./install.sh --no-interactive
-elif [ ! -z $(command -v sudo) ] && [ "$(sudo -v)" = 0 ]; then
-  sudo -H ./install.sh --no-interactive
+elif [ ! -z $(command -v sudo) ]; then
+  sudo -v
+  if [ $? -eq 0 ]; then
+    sudo -H ./install.sh --no-interactive
+  else
+    >&2 echo "Failed to validate sudo"
+    exit 1;
+  fi
 else
   >&2 echo "Cannot run as non-root user without sudo priveledges"
   exit 1;
